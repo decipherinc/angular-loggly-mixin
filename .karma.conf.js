512d7abb499493f867ff3865c1feca13ac98ff18
Christopher Hiller <chiller@decipherinc.com>
2015-12-18 22:52:23 -0800

    fix the damn report name in .karma.conf.js

--- .karma.conf.js
+++ .karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function karmaConfig(config) {
           type: 'text-summary'
         },
         {
-          type: 'lcov-only',
+          type: 'lcovonly',
           subdir: '.'
         }
       ]
